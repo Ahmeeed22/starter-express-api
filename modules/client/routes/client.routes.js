e7792346566de9a26dc0cee3a0edc79e906ef9e8
Ahmeeed22 <78164396+Ahmeeed22@users.noreply.github.com>
2024-04-16 20:02:28 +0200

    add GetAllOwnHistory route

--- modules/client/routes/client.routes.js
+++ modules/client/routes/client.routes.js
@@ -1,6 +1,7 @@
 const clientRoutes= require("express").Router();
 const isAuth = require("../../../common/middleare/isAuth");
-const validateRequest=require('../../../common/middleare/validationRequest')
+const validateRequest=require('../../../common/middleare/validationRequest');
+const { getClientHistorys } = require("../../clinetHistory/controller/clientHistory.controller");
 
 
 const {addClient , getAllClients , getSingleClient , login ,search ,updateClient ,isEmailAvailable ,isIdentityAvailable , isPhoneNumberAvailable } = require("../controllers/client.controller")
@@ -12,6 +13,7 @@ clientRoutes.get("/AllClients",isAuth('ALL'),getAllClients);
 clientRoutes.put('/UpdateClient/:id',isAuth('ALL'),validateRequest(updateClientSchema),updateClient);
 clientRoutes.post('/AddClient',validateRequest(addClientSchema),isAuth('ALL'),addClient); 
 clientRoutes.get('/GetSingleClient/:id',isAuth('ALL'),getSingleClient);
+clientRoutes.get('/GetAllOwnHistory',isAuth('ALL'),getClientHistorys);
 clientRoutes.get('/SearchClient',isAuth('ADMIN'),search);
 clientRoutes.post('/Login',validateRequest(loginSchema),login);
 clientRoutes.post('/IsEmailAvailable',isEmailAvailable) ;
